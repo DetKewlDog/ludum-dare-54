ab9a5e56cd5d1526406eb1224198cb99e794a1fe
Bar Goldenstein <bargolden12@gmail.com>
2023-09-27 16:25:06 +0300

    renamed repo, added scripts folder, added cmd shortcut script

---
+++ ludum-dare-54/Assets/Scripts/Editor/CmdWindow.cs
@@ -0,0 +1,9 @@
+﻿#if UNITY_EDITOR
+using System.Diagnostics;
+using UnityEditor;
+public class CmdWindow : Editor
+{
+    [MenuItem("Tools/Open Command Prompt %`")]
+    public static void OpenConsole() => Process.Start(new ProcessStartInfo("cmd.exe"));
+}
+#endif
